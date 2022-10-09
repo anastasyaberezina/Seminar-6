62c32c4fc75a706d0ed1f9511342927a2a818366
Anastasya <gavrilovaanastasya@yandex.ru>
2022-10-09 21:24:29 +0300

    переворот массива

---
+++ Seminar 6.1/Program.cs
@@ -0,0 +1,45 @@
+﻿// Переворот одномерного массива.
+
+int[] array = MyArray(8, 0, 20);
+Console.WriteLine(String.Join(" ", array));
+
+int[] reversArray = ReversArray2(array);
+Console.WriteLine(String.Join(" ", reversArray));
+
+ReversArray1(array);
+Console.WriteLine(String.Join(" ", reversArray));
+
+//Первый метод невозвратный
+
+void ReversArray1(int[] array)
+{
+    for(int i = 0; i<array.Length/2; i++)
+    {
+        int k = array[i];
+        array[i]=array[array.Length-i-1];
+        array[array.Length-i-1]=k;
+    }
+}
+
+//Метод для записи в новый массив:
+
+int[] ReversArray2(int[] array)
+{
+    int[] result= new int[array.Length];
+    for(int i=0; i<array.Length; i++)
+    {
+        result[i]=array[array.Length-i-1];
+    }
+    return result;
+}
+
+int[] MyArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for(int i=0; i<size; i++)
+    {
+        res[i]= new Random().Next(minValue, maxValue+1);
+    }
+    return res;
+}

70fae5f5a6fc53ab2ce7231895bf1c8070ff617e
Anastasya <gavrilovaanastasya@yandex.ru>
2022-10-09 23:15:31 +0300

    проверяет, может ли существовать треугольник из 3 введенных чисел

---
+++ Seminar 6.2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Принимает на вход 3 числа и проверяет, может ли существовать треугольник со сторонами определенной длины.
+
+
+Console.WriteLine("Введите первое число: ");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int y = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int z = int.Parse(Console.ReadLine());
+
+if( x<(y+z) && y<(x+z) && z<(y+x) )
+{
+Console.WriteLine("Треугольник может существовать");
+}
+else
+{
+Console.WriteLine("Треугольник может существовать");
+}
+
+
+
+
+
+
+

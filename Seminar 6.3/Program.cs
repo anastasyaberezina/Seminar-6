49416bde86c2e39026995d1bd8ae770b8dc28ef9
Anastasya <gavrilovaanastasya@yandex.ru>
2022-10-09 23:48:28 +0300

    преобразовывает десятичное число в двоичное

---
+++ Seminar 6.3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+Console.WriteLine("Введите число: ");
+int x = int.Parse(Console.ReadLine());
+
+
+string y = String.Empty;
+
+while(x>0)
+{
+    int result = x%2;
+    x=x/2;
+    y=y+Convert.ToString(result);
+}
+Console.Write(y);
+
+string k = String.Empty;
+
+for(int i=y.Length-1; i>=0; i--)
+{
+    k=k+y[i];
+}
+Console.Write(k);

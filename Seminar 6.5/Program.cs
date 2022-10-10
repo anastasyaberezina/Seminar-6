b98df6fc307f64c61654f4c78573532bea2957c8
Anastasya <gavrilovaanastasya@yandex.ru>
2022-10-10 10:35:44 +0300

    Программа, создающая копию заданного массива с помощью поэлементного копирования.

---
+++ Seminar 6.5/Program.cs
@@ -0,0 +1,28 @@
+﻿// Программа, создающая копию заданного массива с помощью поэлементного копирования.
+
+int[] array = GetArray(9);
+Console.WriteLine($"{String.Join(", ", array)}");
+
+int[] newarray = ArrayRes(array);
+Console.WriteLine(String.Join(", ", newarray));
+
+int[] ArrayRes (int[] array)
+{
+    int[] res = new int[array.Length];
+    for(int i=0; i<array.Length; i++)
+    {
+        res[i] = array[i];
+    }
+    return res;
+}
+
+int[] GetArray (int size)
+{
+    int[] result = new int[size];
+    for(int i = 0; i<size; i++)
+    {
+        result[i] = new Random().Next(1, 90);
+    }
+    return result;
+}
+

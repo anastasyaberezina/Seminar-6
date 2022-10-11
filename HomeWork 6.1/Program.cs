dc0055e1739184a345a19b914061a4e8b2c90925
Anastasya <gavrilovaanastasya@yandex.ru>
2022-10-11 09:51:29 +0300

    Посчитайте, сколько чисел больше 0 ввёл пользователь.

--- HomeWork 6.1/Program.cs
+++ HomeWork 6.1/Program.cs
@@ -2,33 +2,50 @@
  Посчитайте, сколько чисел больше 0 ввёл пользователь.0, 7, 8, -2, -2 -> 21, -7, 567, 89, 223-> 4*/
 
 Console.WriteLine("Введите первое число: ");
-int x = int.Parse(Console.ReadLine());  
+int a = int.Parse(Console.ReadLine()!);
 
 Console.WriteLine("Введите второе число: ");
-int y = int.Parse(Console.ReadLine());  
+int b = int.Parse(Console.ReadLine()!);
 
 Console.WriteLine("Введите третье число: ");
-int z = int.Parse(Console.ReadLine());  
+int c = int.Parse(Console.ReadLine()!);
 
-int[] mass = {x, y, z};
-int[] newmass = newMass(mass);
-Console.WriteLine(String.Join(", ", newmass));
+Console.WriteLine("Введите четвертое число: ");
+int d = int.Parse(Console.ReadLine()!);
 
+int[] array = {a, b, c, d};
+Console.WriteLine($"{String.Join(", ", array)}");
 
-int[] newMass (int[] mass)
+int[] newarray = ArrayRes(array);
+
+int count1 = Result(newarray);
+Console.WriteLine();
+Console.WriteLine($"Среди введенных чисел больше нуля {count1}");
+
+int[] ArrayRes (int[] array)
 {
-    int[] res = new int[mass.Length];
-    int count =0;
-    for(int i=0; i<mass.Length; i++)
+    int[] res = new int[array.Length];
+    for(int i=0; i<array.Length; i++)
     {
-
-        if(mass[i]>0)
+        if(array[i]>0)
         {
-            res[i] = mass[i];
-            count++;
+        res[i] = array[i];
+        }
+    } 
+    return res;
+}
+
+int Result (int[] newarray)
+{
+    int[] res1 = new int[newarray.Length];
+    int count = 0;
+    for(int w=0; w<newarray.Length; w++)
+    {
+        if(newarray[w]>0)
+        {
+        res1[w] = newarray[w];
+        count++;
         }
     }
     return count;
 }
-
-
